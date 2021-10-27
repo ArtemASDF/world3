ca891fdd31a7a6e8532e779984e67634f20bef76
MatvejTT <trubnikovmatvej4@gmail.com>
2021-10-27 21:30:50 +0300

    урок3.py

--- world3.py
+++ world3.py
@@ -1,1 +1,104 @@
-print("somenthing new qww ")
+# l = list() # []
+# l.append(10)
+# l.append(-3)
+# l.append(25)
+# #l.append('qeqeqeqe1231313')
+# #l.append(1, 2, 2)
+# l.sort()
+# l.reverse()
+# print(l.pop(2))
+# print(l)  
+
+# t = (10, -3, 5)
+
+# # print(t.count())
+
+#  t = ('s', )
+#  print(type(t), t)
+
+# l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+
+# print(9 in l )
+
+# print(hash('qwesdf'))
+# print(hash('oidjfi'))
+#d = dict(a= '12345', b=87654)
+# #     'a': 1234,
+# #     'b': 5678,
+# # }
+# d.update({'c': 768667868})
+# print(d.get('h'))
+
+# s = {1, 2, 3, 4, 5}
+# print(s)
+# # a = 3 
+# # b = 3 
+# # print( a is b )
+# l1 = [1, 2, 4, [5, 6]]
+# l2 = l1.copy()
+# print(l1)
+# # print(l2)
+# # print(l1(-1) is l2(-1) )
+# # print(l1 == l2 )
+# if 10 < 11: 
+#     print('верно')
+# else:
+#     print('Неверно')
+# is_valid  = True 
+# status = 'clean' if is_valid else 'not clean'
+# print( status)
+
+# is_valid = True
+
+# print(int(is_valid))
+# status = ('not clean', 'clean')[is_valid]
+# print(status)
+
+# funs_out = None 
+# msg = funs_out or 'no outrut'
+# print(msg )
+
+# a = input()
+# shop = {
+#     'milk ':10,
+#     'bytter':20,
+#     'tomato':30,
+# }
+# print(shop.get(a, -1))
+
+
+# i = 10 
+# while i > 0:
+#     print(i)
+#     i -=  1
+
+# l1 = {
+#     'a': [1, 2, 3, 4, 5],
+#     'b': 43
+# }
+
+# for x in '1234567890':
+#     if x in ('qweqeqe'):
+#         break 
+#     print(x)
+# else:
+#     print('не было break ')
+
+# print('Конец')
+
+
+# elements = [123,  34533, 3457, 896, 234, 546]
+
+# for i, x in enumerate(elements):
+#     if x % 3 == 0: 
+#         print(i, x ) 
+
+# n = 6
+# l = [1, 1]
+
+# for i in range(2, n+1):
+#     x = l[i-1] + l[i-2]
+#     l.append(x)
+
+# print(l[n]) 
